e8a5427dda89dc2723dd83ca8751eb83180478dc
Harshinig-123 <harshini.reddy101@gmail.com>
2025-12-11 22:39:57 +0530

    11Dec Insertion Sort

---
+++ InsertionSort_11Dec.java
@@ -0,0 +1,16 @@
+class InsertionSort_11Dec {
+    public int[] insertionSort(int[] nums) {
+        for(int i = 1; i<nums.length; i++)
+        {
+            int current = nums[i];
+            int j = i-1;
+            while(j>=0 && current<nums[j])
+            {
+                nums[j+1] = nums[j];
+                j--;
+            }
+            nums[j+1] = current;
+         }
+         return nums;
+    }
+}
